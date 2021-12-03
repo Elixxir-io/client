f0e93775ee851401623af68de783392731fa8654
jbhusson <jonah@elixxir.io>
2021-12-03 13:59:28 -0500

    Basic test for download & verify skipped nodes

--- bindings/skipnodes_test.go
+++ bindings/skipnodes_test.go
@@ -1,1 +1,19 @@
 package bindings
+
+//func TestDownloadAndVerifySkipNodes(t *testing.T) {
+//	ep, err := utils.ExpandPath("/path/to/a/cert")
+//	if err != nil {
+//		t.Errorf("Failed to expand: %+v", err)
+//		t.FailNow()
+//	}
+//	cert, err := utils.ReadFile(ep)
+//	if err != nil {
+//		t.Errorf("Failed to read %s: %+v", ep, err)
+//		t.FailNow()
+//	}
+//	list, err := DownloadAndVerifySkipNodes("https://elixxir-bins.s3.us-west-1.amazonaws.com/client/skipnodes/skipNodes.json", string(cert))
+//	if err != nil {
+//		t.Errorf("Failed to download skipped node: %+v", err)
+//	}
+//	fmt.Println(list)
+//}
