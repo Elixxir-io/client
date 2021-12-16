c1368fbba5d2c71efb24862dfbdf31d0e8163d48
Jono Wenger <jono@elixxir.io>
2021-12-16 15:36:53 -0800

    Add GetFullKey to KV interface

--- storage/versioned/kv.go
+++ storage/versioned/kv.go
@@ -11,9 +11,9 @@ type KV interface {
 	// Prefix returns a new UnbufferedKV with the new prefix.
 	Prefix(prefix string) *UnbufferedKV
 
-	// Set upserts new data into the unbuffered key/value store. When calling this,
-	// you are responsible for prefixing the key with the correct type optionally
-	// unique ID! Call Prefix to do so.
+	// Set upserts new data into the unbuffered key/value store. When calling
+	// this, you are responsible for prefixing the key with the correct type
+	// optionally unique ID! Call Prefix to do so.
 	Set(key string, version uint64, object *Object) error
 
 	// Get gets and upgrades data stored in the unbuffered key/value store. Make
@@ -21,11 +21,15 @@ type KV interface {
 	Get(key string, version uint64) (*Object, error)
 
 	// GetAndUpgrade gets and upgrades data stored in the unbuffered key/value
-	// store. Make sure to inspect the version returned inside the versioned object.
+	// store. Make sure to inspect the version returned inside the versioned
+	// object.
 	GetAndUpgrade(key string, ut UpgradeTable) (*Object, error)
 
 	// Delete removes a given key from the unbuffered key/value store.
 	Delete(key string, version uint64) error
+
+	// GetFullKey returns the key with all prefixes appended.
+	GetFullKey(key string, version uint64) string
 }
 
 // Upgrade functions must be of this type.
